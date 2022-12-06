4ea88caafb3c73d1072cc62c842e55bcb358bfa8
OFFICIALSEBAS <sebastian.bonillac@udea.edu.co>
2022-12-06 11:04:20 -0500

    Implementing Drawer navigation: Understanding it. Drawer menu is openned with drawer icon in action bar. But Drawer menu is opened bellow tool bar.

--- CountriesApp/app/src/main/java/com/example/countriesapp/presentation/MainActivity.kt
+++ CountriesApp/app/src/main/java/com/example/countriesapp/presentation/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.countriesapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -9,19 +12,23 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.countriesapp.R
 import com.example.countriesapp.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var mainBinding: ActivityMainBinding
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
+        val toolBar = mainBinding.activityMainContent.mainToolbar
+
         //Setting up the tool bar as the app bar for the activity
-        setSupportActionBar(mainBinding.activityMainContent.mainToolbar)
+        setSupportActionBar(toolBar)
 
         val host: NavHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
@@ -36,10 +43,18 @@ class MainActivity : AppCompatActivity() {
         )
 
         //First
-        setUpNavigationView(navController)
+        setUpActionBar(navController,appBarConfiguration)
 
         //Second
-        setUpActionBar(navController,appBarConfiguration)
+        toggle = ActionBarDrawerToggle(this,mainBinding.drawerLayout,toolBar,R.string.open_drawer, R.string.close_drawer)
+        mainBinding.drawerLayout.addDrawerListener(toggle)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+
+
+        //Second
+        setUpNavigationView(navController)
 
 
     }
@@ -50,5 +65,10 @@ class MainActivity : AppCompatActivity() {
 
     private fun setUpNavigationView(controller: NavController) {
         mainBinding.navView.setupWithNavController(controller)
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        mainBinding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
