edd9c9db124c5e897ef101b2b44d563196ab20b8
OFFICIALSEBAS <sebastian.bonillac@udea.edu.co>
2023-01-21 08:49:54 -0500

    On show location implementing take-picture functionality: image is apearring flipped

--- CountriesApp/app/src/main/java/com/example/countriesapp/presentation/fragments/tabubication/current/ShowLocationFragment.kt
+++ CountriesApp/app/src/main/java/com/example/countriesapp/presentation/fragments/tabubication/current/ShowLocationFragment.kt
@@ -1,14 +1,23 @@
 package com.example.countriesapp.presentation.fragments.tabubication.current
 
+
+import android.Manifest
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.countriesapp.R
 import com.example.countriesapp.databinding.FragmentShowLocationBinding
 import com.example.countriesapp.presentation.fragments.tabubication.listnearcities.ListNearCityFragment
@@ -16,7 +25,9 @@ import com.example.countriesapp.presentation.models.UbicationClass
 
 class ShowLocationFragment : Fragment() {
 
-
+    private val REQUEST_CODE = 200
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val REQUEST_PERMISSION = 100
     private lateinit var viewModel: ShowLocationViewModel
     private lateinit var binding: FragmentShowLocationBinding
     var ubication: UbicationClass? = null
@@ -35,6 +46,8 @@ class ShowLocationFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        checkCameraPermission()
+
         if (arguments != null) {
 
             ubication = arguments?.getSerializable("current") as UbicationClass
@@ -48,6 +61,9 @@ class ShowLocationFragment : Fragment() {
                 tvLatitude.text = "Latitude: $lat"
                 tvLongitude.text = "longitude: $lon"
                 tvLocality.text = ubication?.locality
+                fabCamera.setOnClickListener {
+                    capturePhoto()
+                }
             }
 
 
@@ -72,5 +88,52 @@ class ShowLocationFragment : Fragment() {
         }
     }
 
+    private fun checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(
+                this@ShowLocationFragment.requireContext(),
+                Manifest.permission.CAMERA
+            )
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this@ShowLocationFragment.requireActivity(),
+                arrayOf(Manifest.permission.CAMERA),
+                1
+            )
+        }
+    }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            1 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if ((ContextCompat.checkSelfPermission(this@ShowLocationFragment.requireContext(),
+                            Manifest.permission.CAMERA) ===
+                                PackageManager.PERMISSION_GRANTED)) {
+                        Toast.makeText(this@ShowLocationFragment.requireContext(), "Permission Granted", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this@ShowLocationFragment.requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
+    private fun capturePhoto() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE && data != null){
+            binding.ivPlaceMemory.setImageBitmap(data.extras?.get("data") as Bitmap)
+        }
+    }
 }
+
+
