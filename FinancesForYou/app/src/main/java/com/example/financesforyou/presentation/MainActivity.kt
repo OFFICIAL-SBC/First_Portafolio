ff6d005fa4e0d33d0c0db15e45ae43f15bbcf8f7
OFFICIALSEBAS <sebastian.bonillac@udea.edu.co>
2023-04-12 09:39:18 -0500

    Im gonna start Finances for you

---
+++ FinancesForYou/app/src/main/java/com/example/financesforyou/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.financesforyou.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.financesforyou.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
